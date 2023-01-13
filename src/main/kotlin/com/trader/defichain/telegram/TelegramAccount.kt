200953ace28150eb0469d0f0bda7192add139abc
Ruben Baas <defichain-trader@tutanota.com>
2023-01-13 22:21:50 +0100

    Add support for "is bought or sold" filters

--- src/main/kotlin/com/trader/defichain/telegram/TelegramAccount.kt
+++ src/main/kotlin/com/trader/defichain/telegram/TelegramAccount.kt
@@ -99,20 +99,32 @@ data class PoolHistoryNotification(
 
     override suspend fun matches(value: Any): Boolean {
         if (value is PoolSwapRow) {
-            if (filter.fromTokenSymbol != null) {
+            println(filter.fromTokenSymbol)
+            println(filter.toTokenSymbol)
+            if(filter.fromTokenSymbol != null && filter.toTokenSymbol == "is_sold_or_bought") {
                 val tokenFrom = getTokenId(value.tokenFrom)
+                val tokenTo = getTokenId(value.tokenTo)
                 val whitelist = getTokenIdentifiers(filter.fromTokenSymbol)
-
-                if (whitelist.isNotEmpty() && !whitelist.contains(tokenFrom)) {
+                println("$tokenFrom $tokenTo ${whitelist.contentToString()}")
+                if (whitelist.isNotEmpty() && !(whitelist.contains(tokenFrom) || whitelist.contains(tokenTo))) {
                     return false
                 }
-            }
-            if (filter.toTokenSymbol != null) {
-                val tokenTo = getTokenId(value.tokenTo)
-                val whitelist = getTokenIdentifiers(filter.toTokenSymbol)
+            } else {
+                if (filter.fromTokenSymbol != null) {
+                    val tokenFrom = getTokenId(value.tokenFrom)
+                    val whitelist = getTokenIdentifiers(filter.fromTokenSymbol)
 
-                if (whitelist.isNotEmpty() && !whitelist.contains(tokenTo)) {
-                    return false
+                    if (whitelist.isNotEmpty() && !whitelist.contains(tokenFrom)) {
+                        return false
+                    }
+                }
+                if (filter.toTokenSymbol != null) {
+                    val tokenTo = getTokenId(value.tokenTo)
+                    val whitelist = getTokenIdentifiers(filter.toTokenSymbol)
+
+                    if (whitelist.isNotEmpty() && !whitelist.contains(tokenTo)) {
+                        return false
+                    }
                 }
             }
 
